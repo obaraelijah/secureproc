8e8614c9074ee2c2e1c298ca455fb5263876dc82
obaraelijah <elijahobara357@gmail.com>
2024-11-04 20:26:11 +0300

    test: memory test

--- pkg/jobmanager/jobmanager.go
+++ pkg/jobmanager/jobmanager.go
@@ -5,11 +5,38 @@ import (
 	"sync"
 
 	"github.com/google/uuid"
+	"github.com/obaraelijah/secureproc/pkg/cgroup/v1"
+	"github.com/obaraelijah/secureproc/pkg/io"
 )
 
 type job struct {
-	mutex sync.Mutex
-	id    uuid.UUID
-	name  string
-	cmd   exec.Cmd
+	mutex        sync.Mutex
+	id           uuid.UUID
+	name         string
+	cgroupSet    *cgroup.Set
+	cmd          exec.Cmd
+	stdoutBuffer io.OutputBuffer
+	stderrBuffer io.OutputBuffer
+}
+
+func NewJob(name string, cgroupSet *cgroup.Set, command string, args ...string) *job {
+	return NewJobDetailed(name, cgroupSet, io.NewMemoryBuffer(), io.NewMemoryBuffer(), command, args...)
+}
+
+func NewJobDetailed(
+	name string,
+	cgroupSet *cgroup.Set,
+	stdoutBuffer io.OutputBuffer,
+	stderrBuffer io.OutputBuffer,
+	command string,
+	args ...string,
+) *job {
+	return &job{
+		id:           uuid.New(),
+		name:         name,
+		cgroupSet:    cgroupSet,
+		cmd:          *exec.Command(""),
+		stdoutBuffer: stdoutBuffer,
+		stderrBuffer: stderrBuffer,
+	}
 }
