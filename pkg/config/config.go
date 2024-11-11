579ddb1939f003f759b0affb1c9184288a9b0776
obaraelijah <elijahobara357@gmail.com>
2024-11-11 12:41:01 +0300

    feat: Update default cgexec location

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,31 @@
 package config
 
-// Note: generally I would avoid having a "config.go" as a place for a bunch of
+import (
+	"os"
+	"path"
+)
+
+// Note: Generally I would avoid having a "config.go" as a place for a bunch of
 //
 //	unrelated constants.  However, here these constants represent the
 //	hard-coded values for the exercise, so I thought this might make it
 //	easier to adjust the values for experimentation if I put them all in
 //	one place.
+var (
+	CgexecPath string
+)
+
+// init sets CgexecPath based on the position of the current executable
+func init() {
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	CgexecPath = path.Dir(exe) + "/cgexec"
+}
+
 const (
-	CgexecPath                   = "/tmp/cgexec"
 	CgroupDefaultCpuLimit        = 0.5
 	CgroupDefaultMemoryLimit     = "2M"
 	CgroupDefaultBlkioDevice     = "8:16"
