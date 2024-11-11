805f5bcfe7fb0ecc05442b9b578b5461dc9bc65e
obaraelijah <elijahobara357@gmail.com>
2024-11-11 12:38:07 +0300

    feat: Use type aliases, move cgroup deletion

--- pkg/adaptation/os/ostest/environ_mock.go
+++ pkg/adaptation/os/ostest/environ_mock.go
@@ -1,11 +1,9 @@
 package ostest
 
 // EnvironMock is a component that provides a mock implementation of the
-// os.Environ() function.  The function returns the configured Environment.
-type EnvironMock struct {
-	Environment []string
-}
+// os.Environ() function.  The function returns the configured Environment.type EnvironMock []string
+type EnvironMock []string
 
-func (e *EnvironMock) Environ() []string {
-	return e.Environment
+func (e EnvironMock) Environ() []string {
+	return e
 }
