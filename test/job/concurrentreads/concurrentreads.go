7e63943e36c3a97820c69ea4cc4adb7f15e632f6
obaraelijah <elijahobara357@gmail.com>
2024-11-12 12:11:44 +0300

    test: Simplify concurrentreads test

--- test/job/concurrentreads/concurrentreads.go
+++ test/job/concurrentreads/concurrentreads.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"io"
-	"strings"
 	"sync"
 
 	"github.com/obaraelijah/secureproc/pkg/jobmanager"
@@ -25,7 +24,7 @@ func runTest() {
 	}
 
 	const numGoroutines = 100
-	var buckets [numGoroutines][]byte
+	var buckets [numGoroutines]bytes.Buffer
 
 	var wg sync.WaitGroup
 	wg.Add(numGoroutines)
@@ -33,7 +32,7 @@ func runTest() {
 	for i := 0; i < numGoroutines; i++ {
 		go func(goroutineNum int) {
 			for output := range job.StdoutStream().Stream() {
-				buckets[goroutineNum] = append(buckets[goroutineNum], output...)
+				buckets[goroutineNum].Write(output)
 			}
 			wg.Done()
 		}(i)
@@ -41,19 +40,14 @@ func runTest() {
 
 	wg.Wait()
 
-	var readers [len(buckets)]*bufio.Reader
-	for i := 0; i < len(readers); i++ {
-		readers[i] = bufio.NewReader(strings.NewReader(string(buckets[i])))
-	}
-
 	for i := 0; i < numValues; i++ {
-		expectedValue, err := readers[0].ReadString('\n')
+		expectedValue, err := buckets[0].ReadString('\n')
 		if err != nil {
 			panic(fmt.Sprintf("Unexpected error at value number %d, goroutine 0: %v", i, err))
 		}
 
-		for j := 1; j < len(readers); j++ {
-			value, err := readers[j].ReadString('\n')
+		for j := 1; j < len(buckets); j++ {
+			value, err := buckets[j].ReadString('\n')
 			if err != nil {
 				panic(fmt.Sprintf("Unexpected error at value number %d, goroutine %d: %v", i, j, err))
 			}
@@ -65,8 +59,8 @@ func runTest() {
 	}
 
 	// There should be no more values; all readers should be at EOF
-	for i := 0; i < len(readers); i++ {
-		_, err := readers[i].ReadString('\n')
+	for i := 0; i < len(buckets); i++ {
+		_, err := buckets[i].ReadString('\n')
 		if err != io.EOF {
 			panic(fmt.Sprintf("Unexpected additional data from goroutine %d", i))
 		}
