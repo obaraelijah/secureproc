a49e2eda02af25eb7b62cb77212a58ad43d98644
obaraelijah <elijahobara357@gmail.com>
2024-11-22 18:41:59 +0300

    feat: Add flag for address

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,13 +8,35 @@ import (
 
 	"github.com/obaraelijah/secureproc/certs"
 	"github.com/obaraelijah/secureproc/pkg/command"
+	"github.com/spf13/cobra"
 )
 
-func main() {
+var (
+	argAddress string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "jobmanager",
+	Short: "Run the job manager server",
+	Run: func(cmd *cobra.Command, args []string) {
+		runServer()
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(
+		&argAddress,
+		"address",
+		"a",
+		":24482",
+		"The <address>:<port> on which this server should listen for incoming requests")
+}
+
+func runServer() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	listener, err := net.Listen("tcp", ":24482")
+	listener, err := net.Listen("tcp", argAddress)
 	if err != nil {
 		panic(err)
 	}
@@ -30,4 +52,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	rootCmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.22.9
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.9.0
 	google.golang.org/grpc v1.68.0
 	google.golang.org/protobuf v1.35.1
@@ -13,7 +14,9 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
