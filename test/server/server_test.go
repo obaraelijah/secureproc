893ec99cd2bbff8c75a77b43478ba8494a730e80
obaraelijah <elijahobara357@gmail.com>
2024-11-25 19:08:23 +0300

    fix: merge error

--- test/server/server_test.go
+++ test/server/server_test.go
@@ -202,7 +202,9 @@ func runServer(
 	go func() {
 		runErr := command.RunJobmanagerServer(
 			ctx, listener, caCert, serverCert, serverKey)
-		assert.Nil(t, runErr)
+		if ruErr != nil {
+			t.Error()
+		}
 		wg.Done()
 	}()
 
